2c31468e12154f78f47627f2f83580765cee01a5
Rody Davis <rody.davis.jr@gmail.com>
2021-05-02 14:26:01 -0700

    adding base

---
+++ types/my-app.d.ts
@@ -0,0 +1,6 @@
+import { LitElement } from "lit";
+export declare class MyApp extends LitElement {
+    static styles: import("lit").CSSResultGroup;
+    name: string;
+    render(): import("lit-html").TemplateResult<1>;
+}
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // base: "/bible/",
+  build: {
+    outDir: "build/web",
+    // lib: {
+    //   entry: "src/bible-app.ts",
+    //   formats: ["es"],
+    // },
+    // manifest: true,
+    rollupOptions: {
+      // external: /^lit-element/,
+      input: {
+        main: resolve(__dirname, "index.html"),
+        // editor: resolve(__dirname, "www/editor/index.html"),
+      },
+    },
+  },
+});
