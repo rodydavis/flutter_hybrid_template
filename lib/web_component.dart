595e40c5319ca63d518ead08139a8f31f9bcfb7f
Rody Davis <rody.davis.jr@gmail.com>
2021-05-02 17:16:16 -0700

    updating

--- lib/web_component.dart
+++ lib/web_component.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-class WebComponent extends StatelessWidget {
+class WebComponent extends StatefulWidget {
   const WebComponent({
     Key key,
     @required this.name,
@@ -15,6 +15,14 @@ class WebComponent extends StatelessWidget {
   final String slot;
   final List<EventCallback> events;
 
+  @override
+  _WebComponentState createState() => _WebComponentState();
+}
+
+class _WebComponentState extends State<WebComponent> {
+  InAppWebViewController controller;
+  final Map<String, List<EventCallback>> _events = {};
+
   String get source {
     return '''<!DOCTYPE html>
 <html lang="en">
@@ -27,20 +35,20 @@ class WebComponent extends StatelessWidget {
         padding: 0;
         margin: 0;
       }
-      $name {
+      ${widget.name} {
         width: 100%;
         height: 100vh;
       }
     </style>
-  <script type="module" crossorigin src="$bundle"></script>
+  <script type="module" crossorigin src="${widget.bundle}"></script>
 </head>
   <body>
-    <$name ${attributes.entries.map((e) => '${e.key}="${e.value}"').join(' ')}>
-      $slot
-    </$name>
+    <${widget.name} ${widget.attributes.entries.map((e) => '${e.key}="${e.value}"').join(' ')}>
+      ${widget.slot}
+    </${widget.name}>
     <script>
     window.addEventListener("flutterInAppWebViewPlatformReady", (event) => {
-      ${events.join('\n')}
+      ${widget.events.join('\n')}
     });
     </script>
   </body>
@@ -48,17 +56,47 @@ class WebComponent extends StatelessWidget {
 ''';
   }
 
+  void _setup(InAppWebViewController controller) {
+    this.controller = controller;
+    this._setupEvents();
+  }
+
+  void _setupEvents() {
+    for (final event in _events.keys) {
+      controller.removeJavaScriptHandler(handlerName: event);
+    }
+    for (final event in widget.events) {
+      _addEvent(event);
+    }
+  }
+
+  void _addEvent(EventCallback event) {
+    controller.addJavaScriptHandler(
+      handlerName: event.query,
+      callback: event.onPressed,
+    );
+    _events[event.event] ??= [];
+    _events[event.event].add(event);
+  }
+
+  @override
+  void didUpdateWidget(covariant WebComponent oldWidget) {
+    if (oldWidget.events != widget.events) {
+      _setupEvents();
+    }
+    if (oldWidget.slot != widget.slot ||
+        oldWidget.bundle != widget.bundle ||
+        oldWidget.name != widget.name) {
+      controller.loadData(data: source);
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
   @override
   Widget build(BuildContext context) {
     return InAppWebView(
       initialData: InAppWebViewInitialData(data: source),
-      onWebViewCreated: (controller) {
-        for (final event in events)
-          controller.addJavaScriptHandler(
-            handlerName: event.query,
-            callback: event.onPressed,
-          );
-      },
+      onWebViewCreated: _setup,
     );
   }
 }
