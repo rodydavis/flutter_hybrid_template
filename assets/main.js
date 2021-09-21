ca31aa05abe034b675f45b1761cf0fd7a523ff8a
rodydavis <rodydavis@users.noreply.github.com>
2021-09-21 18:59:27 +0000

    deploy: 7d86185f50dd43c484d400e7b341d429b8915e3c

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,8 @@
-import{r as e,e as t,n as r,h as o,T as n}from"./vendor.js";!function(e=".",t="__import__"){try{self[t]=new Function("u","return import(u)")}catch(r){const o=new URL(e,location),n=e=>{URL.revokeObjectURL(e.src),e.remove()};self[t]=e=>new Promise(((r,s)=>{const l=new URL(e,o);if(self[t].moduleMap[l])return r(self[t].moduleMap[l]);const p=new Blob([`import * as m from '${l}';`,`${t}.moduleMap['${l}']=m;`],{type:"text/javascript"}),a=Object.assign(document.createElement("script"),{type:"module",src:URL.createObjectURL(p),onerror(){s(new Error(`Failed to import: ${e}`)),n(a)},onload(){r(self[t].moduleMap[l]),n(a)}});document.head.appendChild(a)})),self[t].moduleMap={}}}("/flutter_hybrid_template/assets/");var s=Object.defineProperty,l=Object.getOwnPropertyDescriptor,p=(e,t,r,o)=>{for(var n,p=o>1?void 0:o?l(t,r):t,a=e.length-1;a>=0;a--)(n=e[a])&&(p=(o?n(t,r,p):n(p))||p);return o&&p&&s(t,r,p),p};let a=class extends o{constructor(){super(...arguments),this.name="Somebody"}render(){return n`<div>
+import{r as e,e as r,n as o,y as t,a as n}from"./vendor.js";!function(){const e=document.createElement("link").relList;if(!(e&&e.supports&&e.supports("modulepreload"))){for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver((e=>{for(const o of e)if("childList"===o.type)for(const e of o.addedNodes)"LINK"===e.tagName&&"modulepreload"===e.rel&&r(e)})).observe(document,{childList:!0,subtree:!0})}function r(e){if(e.ep)return;e.ep=!0;const r=function(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),"use-credentials"===e.crossorigin?r.credentials="include":"anonymous"===e.crossorigin?r.credentials="omit":r.credentials="same-origin",r}(e);fetch(e.href,r)}}();var i=Object.defineProperty,s=Object.getOwnPropertyDescriptor;"undefined"!=typeof require&&require;var l=(e,r,o,t)=>{for(var n,l=t>1?void 0:t?s(r,o):r,c=e.length-1;c>=0;c--)(n=e[c])&&(l=(t?n(r,o,l):n(l))||l);return t&&l&&i(r,o,l),l};let c=class extends o{constructor(){super(...arguments),this.name="Somebody"}render(){return t`<div>
       <p>Hello, ${this.name}!</p>
       <slot></slot>
-    </div>`}};a.styles=e`
+    </div>`}};c.styles=e`
     p {
       color: blue;
     }
-  `,p([t()],a.prototype,"name",2),a=p([r("my-app")],a);
+  `,l([r()],c.prototype,"name",2),c=l([n("my-app")],c);
